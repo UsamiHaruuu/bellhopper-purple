ce60b984a4314a0d5c1184b4fcbc68271784112f
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-02-21 16:04:12 -0600

    lint and change empty tags to divs (was messing with my syntax highlighting)

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Column, Button, Container } from "rbx";
-import InfoBox from "./InfoBox";
-import Header from "./Header";
-import ExchangeRate from "./CountryHelpers/ExchangeRate";
+import React from 'react';
+import { Column, Button, Container } from 'rbx';
+import InfoBox from './InfoBox';
+import Header from './Header';
+import ExchangeRate from './CountryHelpers/ExchangeRate';
 
 const Dashboard = ({ country }) => {
   const countryCurrency = ExchangeRate(country);
-  const cc = require("currency-codes");
+  const cc = require('currency-codes');
   const countryCurrencyName = cc.country(country)[0].code;
   const testData = [
     {
-      title: "Weather",
+      title: 'Weather',
       contents: (
-        <>
+        <div>
           <Column.Group breakpoint="mobile">
             <Column size={8}>
               <p>Daytime Hi: 74</p>
@@ -23,13 +23,13 @@ const Dashboard = ({ country }) => {
               <p>light rain</p>
             </Column>
           </Column.Group>
-        </>
-      )
+        </div>
+      ),
     },
     {
-      title: "Travel Warnings",
+      title: 'Travel Warnings',
       contents: (
-        <>
+        <div>
           <p>
             Exercise increased caution in Costa Rica due to crime. (Jan 7, 2020)
           </p>
@@ -39,49 +39,53 @@ const Dashboard = ({ country }) => {
             spread by mosquitoes. Travelers going to Costa Rica should take
             steps to avoid mosquito bites. (Aug 27, 2019)
           </p>
-        </>
-      )
+        </div>
+      ),
     },
     {
-      title: "Visa Requirements",
+      title: 'Visa Requirements',
       contents: (
         <p>
           Not required for stays less than 90 days, but return ticket required.
         </p>
-      )
+      ),
     },
     {
-      title: "Vaccinations",
+      title: 'Vaccinations',
       contents: (
         <p>
           Proof of yellow fever vaccination must be presented upon arrival for
           all passengers coming from certain countries in South America or
           Africa
         </p>
-      )
+      ),
     },
     {
-      title: "Plug Type",
-      contents: <p>A/B type plug, 120v (USA standard electrical system)</p>
+      title: 'Plug Type',
+      contents: <p>A/B type plug, 120v (USA standard electrical system)</p>,
     },
     {
-      title: "Exchange Rate",
+      title: 'Exchange Rate',
       contents: (
         <p>
-          {" "}
-          1 USD = {countryCurrency} {countryCurrencyName}
+          {' '}
+          1 USD =
+          {' '}
+          {countryCurrency}
+          {' '}
+          {countryCurrencyName}
         </p>
-      )
-    }
+      ),
+    },
   ];
 
   return (
     <div>
       <Header country={country} />
-      {testData.map(data => (
+      {testData.map((data) => (
         <InfoBox key={data.title} title={data.title} contents={data.contents} />
       ))}
-      <Container style={{ textAlign: "center" }}>
+      <Container style={{ textAlign: 'center' }}>
         <Button as="a" color="link" size="large" href="/#/search/">
           Return To Search
         </Button>
