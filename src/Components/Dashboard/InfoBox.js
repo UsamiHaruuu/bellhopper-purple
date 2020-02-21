553bace1f1e27a791643572d5fa48ad060e64dec
Paul Farcasanu <pfarcasanu@gmail.com>
2020-02-20 20:43:21 -0600

    collapsing

--- src/Components/Dashboard/InfoBox.js
+++ src/Components/Dashboard/InfoBox.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Message } from 'rbx';
+import React, { useState } from 'react';
+import { Message, Button, Icon } from 'rbx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
-const InfoBox = ({ title, contents }) => (
-  <Message className="info-box">
-    <Message.Header>
-      {title}
-    </Message.Header>
-    <Message.Body>
-      {contents}
-    </Message.Body>
-  </Message>
-);
+const InfoBox = ({ title, contents }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Message className="info-box">
+      <Message.Header>
+        {title}
+        <Button onClick={() => setCollapsed(!collapsed)}>
+          <Icon>
+            <FontAwesomeIcon icon={!collapsed ? faCaretDown : faCaretUp} />
+          </Icon>
+        </Button>
+      </Message.Header>
+      {!collapsed && (
+      <Message.Body>
+        {contents}
+      </Message.Body>
+      )}
+    </Message>
+  );
+};
 
 export default InfoBox;
