58dc5ad129947d05a87e882341f45283ab854271
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-02-21 16:25:55 -0600

    initial restructure

---
+++ src/Components/Dashboard/CountryHelpers/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Column } from 'rbx';
+import ExchangeRate from './ExchangeRate';
+// import TravelAdvisory from './TravelAdvisory';
+
+const getCountryData = (country) => {
+  const countryCurrency = ExchangeRate(country);
+  const cc = require('currency-codes');
+  const countryCurrencyName = cc.country(country)[0].code;
+
+  return [
+    {
+      title: 'Weather',
+      contents: (
+        <div>
+          <Column.Group breakpoint="mobile">
+            <Column size={8}>
+              <p>Daytime Hi: 74</p>
+              <p>Daytime Lo: 57</p>
+            </Column>
+            <Column size={4}>
+              <p className="degrees-text">68 &deg;F</p>
+              <p>light rain</p>
+            </Column>
+          </Column.Group>
+        </div>
+      ),
+    },
+    {
+      title: 'Travel Warnings',
+      contents: (
+        <div>
+          <p>
+              Exercise increased caution in Costa Rica due to crime. (Jan 7, 2020)
+          </p>
+          <br />
+          <p>
+              Outbreak alert: There is a dengue outbreak in Costa Rica. Dengue is
+              spread by mosquitoes. Travelers going to Costa Rica should take
+              steps to avoid mosquito bites. (Aug 27, 2019)
+          </p>
+        </div>
+      ),
+    },
+    {
+      title: 'Visa Requirements',
+      contents: (
+        <p>
+            Not required for stays less than 90 days, but return ticket required.
+        </p>
+      ),
+    },
+    {
+      title: 'Vaccinations',
+      contents: (
+        <p>
+            Proof of yellow fever vaccination must be presented upon arrival for
+            all passengers coming from certain countries in South America or
+            Africa
+        </p>
+      ),
+    },
+    {
+      title: 'Plug Type',
+      contents: <p>A/B type plug, 120v (USA standard electrical system)</p>,
+    },
+    {
+      title: 'Exchange Rate',
+      contents: (
+        <p>
+          {' '}
+            1 USD =
+          {' '}
+          {countryCurrency}
+          {' '}
+          {countryCurrencyName}
+        </p>
+      ),
+    },
+  ];
+};
+
+export default getCountryData;
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,97 +1,25 @@
 import React from 'react';
-import { Column, Button, Container } from 'rbx';
+import { Button, Container } from 'rbx';
 import InfoBox from './InfoBox';
 import Header from './Header';
-import ExchangeRate from './CountryHelpers/ExchangeRate';
+import getCountryData from './CountryHelpers';
 
-const Dashboard = ({ country }) => {
-  const countryCurrency = ExchangeRate(country);
-  const cc = require('currency-codes');
-  const countryCurrencyName = cc.country(country)[0].code;
-  const testData = [
-    {
-      title: 'Weather',
-      contents: (
-        <div>
-          <Column.Group breakpoint="mobile">
-            <Column size={8}>
-              <p>Daytime Hi: 74</p>
-              <p>Daytime Lo: 57</p>
-            </Column>
-            <Column size={4}>
-              <p className="degrees-text">68 &deg;F</p>
-              <p>light rain</p>
-            </Column>
-          </Column.Group>
-        </div>
-      ),
-    },
-    {
-      title: 'Travel Warnings',
-      contents: (
-        <div>
-          <p>
-            Exercise increased caution in Costa Rica due to crime. (Jan 7, 2020)
-          </p>
-          <br />
-          <p>
-            Outbreak alert: There is a dengue outbreak in Costa Rica. Dengue is
-            spread by mosquitoes. Travelers going to Costa Rica should take
-            steps to avoid mosquito bites. (Aug 27, 2019)
-          </p>
-        </div>
-      ),
-    },
-    {
-      title: 'Visa Requirements',
-      contents: (
-        <p>
-          Not required for stays less than 90 days, but return ticket required.
-        </p>
-      ),
-    },
-    {
-      title: 'Vaccinations',
-      contents: (
-        <p>
-          Proof of yellow fever vaccination must be presented upon arrival for
-          all passengers coming from certain countries in South America or
-          Africa
-        </p>
-      ),
-    },
-    {
-      title: 'Plug Type',
-      contents: <p>A/B type plug, 120v (USA standard electrical system)</p>,
-    },
-    {
-      title: 'Exchange Rate',
-      contents: (
-        <p>
-          {' '}
-          1 USD =
-          {' '}
-          {countryCurrency}
-          {' '}
-          {countryCurrencyName}
-        </p>
-      ),
-    },
-  ];
+const urlParams = new URLSearchParams(window.location.search);
+const countryName = urlParams.get('country');
+const countryData = getCountryData(countryName);
 
-  return (
-    <div>
-      <Header country={country} />
-      {testData.map((data) => (
-        <InfoBox key={data.title} title={data.title} contents={data.contents} />
-      ))}
-      <Container style={{ textAlign: 'center' }}>
-        <Button as="a" color="link" size="large" href="/#/search/">
+const Dashboard = () => (
+  <div>
+    <Header country={countryName} />
+    {countryData.map((data) => (
+      <InfoBox key={data.title} title={data.title} contents={data.contents} />
+    ))}
+    <Container style={{ textAlign: 'center' }}>
+      <Button as="a" color="link" size="large" href="/#/search/">
           Return To Search
-        </Button>
-      </Container>
-    </div>
-  );
-};
+      </Button>
+    </Container>
+  </div>
+);
 
 export default Dashboard;
