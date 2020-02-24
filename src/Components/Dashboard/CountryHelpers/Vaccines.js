a76a908dceac3a8199f7e977e780cb88e24b4067
Paul Farcasanu <pfarcasanu@gmail.com>
2020-02-23 22:04:05 -0600

    linted code, added Vaccines

---
+++ src/Components/Dashboard/CountryHelpers/Vaccines.js
@@ -0,0 +1,16 @@
+const Vaccines = async (code) => {
+  const url = `https://api.tugo.com/v1/travelsafe/countries/${code}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-Auth-API-Key': '7bq7gsk46t2dw37j4dcaaccu',
+    },
+  });
+  const res = await response.json();
+  const vaccinesArray = res.health.diseasesAndVaccinesInfo.Vaccines;
+  return vaccinesArray.slice(2, vaccinesArray.length).map((v) => v.category).join(', ');
+};
+
+export default Vaccines;
--- src/Components/Dashboard/CountryHelpers/index.js
+++ src/Components/Dashboard/CountryHelpers/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Column } from "rbx";
-import ExchangeRate from "./ExchangeRate";
-import TravelAdvisory from "./TravelAdvisory";
-import moment from "moment";
-import PlugType from "./PlugType";
-const cc = require("currency-codes");
+import React from 'react';
+import { Column } from 'rbx';
+import moment from 'moment';
+import ExchangeRate from './ExchangeRate';
+import TravelAdvisory from './TravelAdvisory';
+import PlugType from './PlugType';
+
+const cc = require('currency-codes');
 
 const getCountryData = async (country, setCountryData) => {
   const countryCurrency = await ExchangeRate(country);
@@ -13,7 +14,7 @@ const getCountryData = async (country, setCountryData) => {
   const travelAdvice = await TravelAdvisory(country);
   setCountryData([
     {
-      title: "Weather",
+      title: 'Weather',
       contents: (
         <div>
           <Column.Group breakpoint="mobile">
@@ -27,14 +28,17 @@ const getCountryData = async (country, setCountryData) => {
             </Column>
           </Column.Group>
         </div>
-      )
+      ),
     },
     {
-      title: "Travel Warnings",
+      title: 'Travel Warnings',
       contents: (
         <div>
           <p>
-            {travelAdvice} ({moment().format("YYYY-MM-DD")} }
+            {travelAdvice}
+            {' '}
+            {moment().format('YYYY-MM-DD')}
+            {' '}
           </p>
           <p>
             Exercise increased caution in Costa Rica due to crime. (Jan 7, 2020)
@@ -46,44 +50,56 @@ const getCountryData = async (country, setCountryData) => {
             steps to avoid mosquito bites. (Aug 27, 2019)
           </p>
         </div>
-      )
+      ),
     },
     {
-      title: "Visa Requirements",
+      title: 'Visa Requirements',
       contents: (
         <p>
           Not required for stays less than 90 days, but return ticket required.
         </p>
-      )
+      ),
     },
     {
-      title: "Vaccinations",
+      title: 'Vaccinations',
       contents: (
         <p>
           Proof of yellow fever vaccination must be presented upon arrival for
           all passengers coming from certain countries in South America or
           Africa
         </p>
-      )
+      ),
     },
     {
-      title: "Plug Type",
+      title: 'Plug Type',
       contents: (
         <p>
-          <p>Avaliable types: {countryPlugData.type.join(" , ")}</p>
-          <p>Avaliable volts: {countryPlugData.volt.join(" , ")}</p>
+          <p>
+            Avaliable types:
+            {' '}
+            {countryPlugData.type.join(' , ')}
+          </p>
+          <p>
+            Avaliable volts:
+            {' '}
+            {countryPlugData.volt.join(' , ')}
+          </p>
         </p>
-      )
+      ),
     },
     {
-      title: "Exchange Rate",
+      title: 'Exchange Rate',
       contents: (
         <p>
-          {" "}
-          1 USD = {countryCurrency[0].toFixed(2)} {countryCurrencyName}
+          {' '}
+          1 USD =
+          {' '}
+          {countryCurrency[0].toFixed(2)}
+          {' '}
+          {countryCurrencyName}
         </p>
-      )
-    }
+      ),
+    },
   ]);
 };
 
