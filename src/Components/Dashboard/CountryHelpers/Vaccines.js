eb4612231e7bf211f67b9a853682fad9fe5f4a75
Paul Farcasanu <pfarcasanu@gmail.com>
2020-02-23 22:12:49 -0600

    try catch

--- src/Components/Dashboard/CountryHelpers/Vaccines.js
+++ src/Components/Dashboard/CountryHelpers/Vaccines.js
@@ -1,16 +1,20 @@
 const Vaccines = async (code) => {
-  const url = `https://api.tugo.com/v1/travelsafe/countries/${code}`;
-  const response = await fetch(url, {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'X-Auth-API-Key': '7bq7gsk46t2dw37j4dcaaccu',
-    },
-  });
-  const res = await response.json();
-  const vaccinesArray = res.health.diseasesAndVaccinesInfo.Vaccines;
-  return vaccinesArray.slice(2, vaccinesArray.length).map((v) => v.category).join(', ');
+  try {
+    const url = `https://api.tugo.com/v1/travelsafe/countries/${code}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Auth-API-Key': '7bq7gsk46t2dw37j4dcaaccu',
+      },
+    });
+    const res = await response.json();
+    const vaccinesArray = res.health.diseasesAndVaccinesInfo.Vaccines;
+    return vaccinesArray.slice(2, vaccinesArray.length).map((v) => v.category).join(', ');
+  } catch {
+    return 'No vaccination information could be found';
+  }
 };
 
 export default Vaccines;
