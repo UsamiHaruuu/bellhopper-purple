fbe8170ea79f3205eec0d8ee103ff2ae20277d2d
Paul Farcasanu <pfarcasanu@gmail.com>
2020-02-24 14:08:02 -0600

    fixed static country

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import 'rbx/index.css';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Search from './Components/Search';
-import TravelAdvisory from './Components/Dashboard/CountryHelpers/TravelAdvisory';
 
 function App() {
-  TravelAdvisory('Germany');
+  const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
+  const country = urlParams.get('country');
   return (
     <div style={{ padding: 20 }}>
       <HashRouter className="padded">
         <Switch>
           <Route exact path="/dashboard">
-            <Dashboard country="Costa Rica" />
+            <Dashboard country={country} />
           </Route>
           <Route exact path="/search">
             <Search />
