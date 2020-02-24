422549033889810f08be08002a3fce95484c5ed6
Paul Farcasanu <pfarcasanu@gmail.com>
2020-02-23 22:04:41 -0600

    linting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import "rbx/index.css";
-import { HashRouter, Route, Switch } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import Search from "./Components/Search";
-// import TravelAdvisory from './Components/Dashboard/CountryHelpers/TravelAdvisory';
-import { PlugType } from "./Components/Dashboard/CountryHelpers/PlugType";
+import React from 'react';
+import 'rbx/index.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+import Search from './Components/Search';
+
 function App() {
-  // TravelAdvisory('Germany');
-  const urlParams = new URLSearchParams(window.location.href.split("?")[1]);
-  const country = urlParams.get("country");
+  const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
+  const country = urlParams.get('country');
   return (
     <div style={{ padding: 20 }}>
       <HashRouter className="padded">
