9cba160bc8ff7c3c86d5b7561284a8b152f4fece
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-02-29 20:48:44 -0600

    clean up weather implementation a bit

--- src/Components/Dashboard/CountryHelpers/Weather.js
+++ src/Components/Dashboard/CountryHelpers/Weather.js
@@ -30,55 +30,15 @@ const getForecasts = async (longitude, latitude) => {
 
   const weatherRes = await weatherResponse.json();
   const weatherObj = {};
-  let i;
-  let dailyWeather;
 
-  for (i = 0; i < 7; i += 1) {
-    dailyWeather = [];
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].maxtempF));
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].mintempF));
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].avgtempF));
-    dailyWeather.push(weatherRes.data.weather[i].date);
-    weatherObj[i] = dailyWeather;
-  }
-  const arr = Object.values(weatherObj);
-
-  return (
-    <div>
-      {
-        arr.map((_, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Column.Group key={index}>
-            <Column size={3}>
-              <Message.Header>
-                {weatherObj[index][3]}
-              </Message.Header>
-              <p>
-                Daytime Hi:
-                {' '}
-                {weatherObj[index][0]}
-                {' '}
-                &deg;F
-              </p>
-              <p>
-                Daytime Low:
-                {' '}
-                {weatherObj[index][1]}
-                {' '}
-                &deg;F
-              </p>
-              <p className="degrees-text">
-                {' '}
-                {weatherObj[index][2]}
-                {' '}
-                &deg;F
-              </p>
-            </Column>
-          </Column.Group>
-        ))
-      }
-    </div>
-  );
+  weatherRes.data.weather.forEach((weather) => {
+    weatherObj[weather.date] = {
+      maxtempF: weather.maxtempF,
+      mintempF: weather.mintempF,
+      avgtempF: weather.avgtempF,
+    };
+  });
+  return weatherObj;
 };
 
 const getHistoricalData = async (longitude, latitude, startDate) => {
@@ -114,94 +74,86 @@ const getHistoricalData = async (longitude, latitude, startDate) => {
 
   const weatherRes = await weatherResponse.json();
   const weatherObj = {};
-  let i;
-  let dailyWeather;
 
-  for (i = 0; i < 7; i += 1) {
-    dailyWeather = [];
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].maxtempF));
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].mintempF));
-    dailyWeather.push(Math.round(weatherRes.data.weather[i].avgtempF));
-    dailyWeather.push(weatherRes.data.weather[i].date);
-    weatherObj[i] = dailyWeather;
-  }
-  const arr = Object.values(weatherObj);
-
-  return (
-    <div>
-      {
-        arr.map((_, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Column.Group key={index}>
-            <Column size={3}>
-              <Message.Header>
-                {weatherObj[index][3]}
-              </Message.Header>
-              <p>
-                Daytime Hi:
-                {' '}
-                {weatherObj[index][0]}
-                {' '}
-                &deg;F
-              </p>
-              <p>
-                Daytime Low:
-                {' '}
-                {weatherObj[index][1]}
-                {' '}
-                &deg;F
-              </p>
-              <p className="degrees-text">
-                {' '}
-                {weatherObj[index][2]}
-                {' '}
-                &deg;F
-              </p>
-            </Column>
-          </Column.Group>
-        ))
-      }
-    </div>
-  );
+  weatherRes.data.weather.forEach((weather) => {
+    weatherObj[weather.date] = {
+      maxtempF: weather.maxtempF,
+      mintempF: weather.mintempF,
+      avgtempF: weather.avgtempF,
+    };
+  });
+  return weatherObj;
 };
 
 const Weather = async (country, city, startDate) => {
   let latitude;
   let longitude;
-  try {
-    if (!city) {
-      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${country}.json?access_token=pk.eyJ1IjoiY2xlbWVuc3RpZ2F0b3IiLCJhIjoiY2p6dm8xeWowMHM0djNnbG02ZWM5ZHo4dSJ9.GNbHIUUjyUdJfazjBuExmw&limit=1`;
-      const response = await fetchWithTimeout(url, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-      const res = await response.json();
-      [longitude, latitude] = res.features[0].center;
-    } else {
-      [longitude, latitude] = [getCityLng(city, country), getCityLat(city, country)];
-    }
-    latitude = parseFloat(latitude);
-    longitude = parseFloat(longitude);
-
-    if (new Date(startDate).getTime() < Date.now() + 86400000 * 7) {
-      return {
-        title: 'Weather',
-        contents: await getForecasts(longitude, latitude),
-      };
-    }
-    return {
-      title: 'Weather (from last year)',
-      contents: await getHistoricalData(longitude, latitude, startDate),
-    };
-  } catch {
-    return {
-      title: 'Weather',
-      contents: 'No information found.',
-    };
+  // try {
+  if (!city) {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${country}.json?access_token=pk.eyJ1IjoiY2xlbWVuc3RpZ2F0b3IiLCJhIjoiY2p6dm8xeWowMHM0djNnbG02ZWM5ZHo4dSJ9.GNbHIUUjyUdJfazjBuExmw&limit=1`;
+    const response = await fetchWithTimeout(url, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    const res = await response.json();
+    [longitude, latitude] = res.features[0].center;
+  } else {
+    [longitude, latitude] = [getCityLng(city, country), getCityLat(city, country)];
   }
+  latitude = parseFloat(latitude);
+  longitude = parseFloat(longitude);
+
+  const retObj = {};
+  let weather;
+  if (new Date(startDate).getTime() < Date.now() + 86400000 * 7) {
+    retObj.title = 'Weather';
+    weather = await getForecasts(longitude, latitude);
+  } else {
+    retObj.title = 'Weather (from last year)';
+    weather = await getHistoricalData(longitude, latitude, startDate);
+  }
+
+  retObj.contents = (
+    <Column.Group>
+      {Object.keys(weather).map((date) => (
+        <Column size={3} key={date}>
+          <Message.Header>
+            {date}
+          </Message.Header>
+          <p>
+            Daytime Hi:
+            {' '}
+            {weather[date].maxtempF}
+            {' '}
+            &deg;F
+          </p>
+          <p>
+            Daytime Low:
+            {' '}
+            {weather[date].mintempF}
+            {' '}
+            &deg;F
+          </p>
+          <p className="degrees-text">
+            {' '}
+            {weather[date].avgtempF}
+            {' '}
+            &deg;F
+          </p>
+        </Column>
+      ))}
+    </Column.Group>
+  );
+  return retObj;
+  // } catch {
+  //   return {
+  //     title: 'Weather',
+  //     contents: 'No information found.',
+  //   };
+  // }
 };
 
 export default Weather;
