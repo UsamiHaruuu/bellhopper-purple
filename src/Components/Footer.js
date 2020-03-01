bced3b019585009d68ea219a19cc620bfe31fb00
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-02-29 20:11:29 -0600

    lint

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Navbar, Icon, Column, Level } from 'rbx';
+import React from 'react';
+import {
+  Navbar, Icon, Level,
+} from 'rbx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faColumns, faList } from '@fortawesome/free-solid-svg-icons';
 
-const Footer = () => {
-  return (
-      <Navbar fixed = "bottom" color = "warning" transparent>
-        <Level>
-          <Level.Item textAlign = "centered">
-            <Navbar.Item href="#">
-              <Icon>
-                <FontAwesomeIcon icon={faHome} size="lg" />
-              </Icon>
-            </Navbar.Item>
-            <Navbar.Item href="#">
-              <Icon>
-                <FontAwesomeIcon icon={faColumns} size="lg" />
-              </Icon>
-            </Navbar.Item>
-            <Navbar.Item href="/#/list">
-              <Icon>
-                <FontAwesomeIcon icon={faList} size="lg" />
-              </Icon>
-            </Navbar.Item>
-          </Level.Item>
-        </Level>
-      </Navbar>
-  );
-};
+const Footer = () => (
+  <Navbar fixed="bottom" color="warning" transparent>
+    <Level>
+      <Level.Item textAlign="centered">
+        <Navbar.Item href="#">
+          <Icon>
+            <FontAwesomeIcon icon={faHome} size="lg" />
+          </Icon>
+        </Navbar.Item>
+        <Navbar.Item href="#">
+          <Icon>
+            <FontAwesomeIcon icon={faColumns} size="lg" />
+          </Icon>
+        </Navbar.Item>
+        <Navbar.Item href="/#/list">
+          <Icon>
+            <FontAwesomeIcon icon={faList} size="lg" />
+          </Icon>
+        </Navbar.Item>
+      </Level.Item>
+    </Level>
+  </Navbar>
+);
 
-export default Footer
+export default Footer;
