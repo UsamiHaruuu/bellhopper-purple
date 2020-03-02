f96a688df1ea350a601038139adc6ff23c9d036e
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-03-01 23:31:27 -0600

    store uuids in the db

--- src/Firebase/helpers.js
+++ src/Firebase/helpers.js
@@ -5,6 +5,13 @@ import firebaseConfig from './config';
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref();
 
+const saveUuid = (uuid) => {
+  db.child(uuid)
+    .set({ trips: false })
+    .catch((error) => alert(error));
+};
+
 export {
+  saveUuid,
   db,
 };
