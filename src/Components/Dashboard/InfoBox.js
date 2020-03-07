813e6ad386fd826feb891caee1170d225caece8d
Paul Farcasanu <pfarcasanu@gmail.com>
2020-03-07 11:33:44 -0600

    check buttons

--- src/Components/Dashboard/InfoBox.js
+++ src/Components/Dashboard/InfoBox.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import { Message, Button, Icon } from 'rbx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCaretUp, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCaretDown, faCaretUp, faPlusSquare, faCheck,
+} from '@fortawesome/free-solid-svg-icons';
 import { addToList } from '../List/helpers';
 
 const InfoBox = ({
   title, contents, uuid, tripId, trip, todo,
 }) => {
   const [collapsed, setCollapsed] = useState(false);
+  const [added, setAdded] = useState(false);
+
+  const addClick = () => {
+    addToList(uuid, tripId, trip, todo);
+    setAdded(true);
+  };
 
   return (
     <Message className="info-box">
       <Message.Header>
         {title}
         <div>
-          <Button onClick={() => addToList(uuid, tripId, trip, todo)}>
+          <Button
+            onClick={() => addClick()}
+            color={!added ? 'light' : 'success'}
+          >
             <Icon>
-              <FontAwesomeIcon icon={faPlusSquare} />
+              <FontAwesomeIcon icon={!added ? faPlusSquare : faCheck} />
             </Icon>
           </Button>
           <Button
