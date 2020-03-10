db59682e232ad02920f03b16ada89ccfa310b99e
Paul Farcasanu <pfarcasanu@gmail.com>
2020-03-10 01:36:34 -0500

    changed an icon

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import {
 } from 'rbx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faPassport, faNotesMedical, faPlug, faMoneyBill,
+  faPassport, faNotesMedical, faPlug, faMoneyBillWave,
 } from '@fortawesome/free-solid-svg-icons';
 import InfoBox from './InfoBox';
 import Header from './Header';
@@ -16,7 +16,7 @@ const DataToIcon = {
   // eslint-disable-next-line quote-props
   'Health': faNotesMedical,
   'Plug Type': faPlug,
-  'Exchange Rate': faMoneyBill,
+  'Exchange Rate': faMoneyBillWave,
 };
 
 const DataLevel = ({ countryData, selected, setSelected }) => (
