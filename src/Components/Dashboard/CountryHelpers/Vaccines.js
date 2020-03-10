871f42af696adbe2d724986faa7eb8aad7194050
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-03-09 23:17:32 -0500

    fix country codes in country data json

--- src/Components/Dashboard/CountryHelpers/Vaccines.js
+++ src/Components/Dashboard/CountryHelpers/Vaccines.js
@@ -1,11 +1,13 @@
 import fetchWithTimeout from './fetchWithTimeout';
+import { getCountryCode } from './CountryDataHelpers';
 
 const Vaccines = async (country) => {
   try {
+    const code = getCountryCode(country);
     const url = 'countrytravelinfo.json';
     const response = await fetchWithTimeout(url);
     const ret = await response.json();
-    return ret.filter((data) => data.geopoliticalarea === country)[0].health;
+    return ret.filter((data) => data.tag === code)[0].health;
   } catch {
     return 'No information found.';
   }
