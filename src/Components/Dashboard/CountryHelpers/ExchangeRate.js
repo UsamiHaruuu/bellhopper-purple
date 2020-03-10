b5e4494f1e7c4a664bce9fc88fa992d7964e5a26
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-03-09 19:17:00 -0500

    update currency exchange api key and return format

--- src/Components/Dashboard/CountryHelpers/ExchangeRate.js
+++ src/Components/Dashboard/CountryHelpers/ExchangeRate.js
@@ -2,23 +2,26 @@ const cc = require('currency-codes');
 
 const ExchangeRate = async (country) => {
   const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-  const countryCurrencyName = cc.country(country);
+  const countryCode = cc.country(country)[0].code;
 
-  if (countryCurrencyName.length !== 0) {
+  if (countryCode) {
     try {
-      // aace65e3a1e550dcca0224927c3b1c88
-      // dc96088f8fb3d56119137a8b10268a62
-      const apiURL = `http://api.currencylayer.com/live?access_key=aace65e3a1e550dcca0224927c3b1c88&currencies=${
-        countryCurrencyName[0].code
-        }&format=1`;
+      const apiKey = 'e5c05625064153f4e2cf1ec9df36fbb8';
+      const apiURL = `http://api.currencylayer.com/live?access_key=${apiKey}&currencies=${
+        countryCode
+      }&format=1`;
       const res = await fetch(proxyurl + apiURL);
       const response = await res.json();
-      return Object.values(response.quotes);
+
+      return {
+        rate: Object.values(response.quotes)[0],
+        countryCode,
+      };
     } catch {
-      return [];
+      return {};
     }
   }
-  return [];
+  return {};
 };
 
 export default ExchangeRate;
